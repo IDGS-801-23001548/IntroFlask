fe2fe0233d21db31d2c8ec05d0f437407bbcb55a
IDGS-801-23001548 <84168@alumnos.utleon.edu.mx>
2026-01-26 18:57:04 -0600

    macros

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hola, Mundo!"
+    titulo = "Flask IDGS-801"
+    lista = ["Emmanuel","Haziel","Emiliano","Flor"]
+    return render_template("index.html", titulo=titulo, lista=lista)
+
+@app.route("/operasBas", methods=['GET','POST'])
+def operasBas():
+    n1=0
+    n2=0
+    res=0
+    if request.method=='POST':
+        n1 = request.form.get('n1')
+        n2 = request.form.get('n2')
+        res = float(n1) + float(n2)
+    return render_template("operasBas.html", n1=n1,n2=n2,res=res)
+
+@app.route("/resultado", methods=['GET','POST'])
+def resultado():
+    n1 = request.form.get('n1')
+    n2 = request.form.get('n2')
+    tem = float(n1) + float(n2)
+    return f'La suma de {n1} + {n2} es: {tem}'
+
+@app.route("/alumnos")
+def alumnos():
+    return render_template("alumnos.html")
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios.html")
 
 @app.route("/hola")
 def hola():
